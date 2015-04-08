c1ef63b82bc94ba9cfe7e9d0666378734d70f0fa
yonh <azssjli@163.com>
2015-04-09 01:26:53 +0800

    init

---
+++ Dockerfile
@@ -0,0 +1,30 @@
+# Version 0.0.1
+# build
+# sudo docker build -t yonh/sinatra:mytools
+# run
+# sudo docker run -it --rm -p 4567:4567 yonh/sinatra:mytools
+# sudo docker run -it --rm -p 4567:4567 -v ~/git/mytools/app:/opt/webapp yonh/sinatra:mytools
+
+FROM ubuntu:14.04
+
+#镜像作者和email
+MAINTAINER yonh "azssjli@163.com"
+ENV REFERSHED_AT 2015-04-01
+
+#执行命令，docker会在每条run指令后创建一个新的镜像层
+#使用官方源实在是太慢了，下载软件还会失败，更改阿里源作替换
+ADD sources.list /etc/apt/sources.list
+RUN apt-get update
+RUN apt-get -y install ruby ruby-dev
+#移除官方源
+#RUN gem sources --remove https://rubygems.org/
+#增加http://ruby.taobao.org/源
+#RUN gem sources -a https://ruby.taobao.org/
+RUN gem install --no-rdoc --no-ri sinatra rqrcode_png
+#ADD app /opt/webapp/
+RUN mkdir -p /opt/webapp
+
+#公开端口
+EXPOSE 4567
+
+CMD [ "/opt/webapp/run" ]
---
+++ app/app.rb
@@ -0,0 +1,68 @@
+# -*- coding: utf-8 -*-
+require 'sinatra'
+require 'rqrcode_png'
+require 'base64'
+require 'digest/md5'
+require 'digest/sha1'
+require "socket"
+
+class String
+	  def md5
+	    Digest::MD5.digest self
+	  end
+	  def md5hex
+	    Digest::MD5.hexdigest self
+	  end
+	  def sha1
+	    Digest::SHA1.hexdigest self
+	  end
+	  def base64
+	    Base64.encode64 self
+	  end
+	end
+class App < Sinatra::Application
+	get '/' do
+	  erb :"index"
+	end
+
+	# http://localhost:4567/qrcode?url=http://baidu.com
+	# 生成制定路径的二维码
+	get '/qrcode' do
+	  erb :'/qrcode/index'
+	end
+	post '/qrcode' do
+	  @qrimg = qrcode(params[:url])
+	  '<img src="data:image/png;base64,'+qrcode(params[:url])+'"/><a href="/">返回</a><form action="/qrcode" method="post"><textarea name="url"></textarea><input value="生成" type="submit" /></form>'
+	end
+
+	def qrcode(msg)
+	  qr = RQRCode::QRCode.new( msg, :size => 19, :level => :h )
+	  Base64.encode64( qr.to_img.resize(400,400).to_s )
+	  #"hello"
+	    #如果要保存的话，如下
+	    #qr.to_img.resize(400, 400).save("my_qrcode.png")
+	end
+
+	
+
+	get '/html' do
+	  erb :'html/index'
+	end
+
+	get '/hash' do
+	  erb :'hash/index'
+	end
+
+	post '/hash' do
+	  @md5 = params[:key].md5hex
+	  @base64 = params[:key].base64
+	  @sha1 = params[:key].sha1
+	  erb :"hash/show"
+	#  text = "MD5:&nbsp;" + params[:key].md5hex + "<br/>"
+	#  text +="SHA1:&nbsp;" + params[:key].sha1 + "<br/>"
+	#  text +="BASE64:&nbsp;" + params[:key].base64
+	#    source = Base64.decode64(code)
+
+	#  text
+	end
+end
